d1b371ed21ec56cb6822a141f6ba5af9b5ef5630
zhaojianghua <zhaojianghua1990@qq.com>
2017-09-19 21:18:00 +0800

    add alg

--- alg.py
+++ alg.py
@@ -4,11 +4,6 @@
 from __future__ import print_function
 
 from collections import Counter
-from copy import deepcopy
-
-
-def edit_distance(word1, word2):
-    pass
 
 
 def _do_permutation(counter, length=0, result=None):
@@ -38,5 +33,27 @@ def permutation(lst):
     _do_permutation(count)
 
 
+def edit_distance(word1, word2):
+    len1, len2 = len(word1), len(word2)
+    table = [[0 for _ in range(len2 + 1)] for _ in range(len1 + 1)]
+    for i in range(len1):
+        table[i + 1][0] = i + 1
+    for j in range(len2):
+        table[0][j + 1] = j + 1
+
+    for i in range(len1):
+        for j in range(len2):
+            fij = 0 if word1[i] == word2[j] else 1
+            table[i + 1][j + 1] = min(table[i + 1][j] + 1,
+                                      table[i][j + 1] + 1,
+                                      table[i][j] + fij)
+
+    return table[len1][len2]
+
+
 if __name__ == "__main__":
-    permutation([1, 1, 1, 1])
+    print("permutation of number list [1, 1, 2]:")
+    permutation([1, 1, 2])
+
+    dis = edit_distance("kitten", "sitting")
+    print("edit distance between 'kitten' and 'sitting' is %s." % dis)
